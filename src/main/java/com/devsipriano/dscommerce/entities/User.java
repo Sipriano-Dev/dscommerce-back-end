772c2c20fcb8908ee93f9bb033d866ccafd25d89
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-29 19:05:57 -0300

    Adicionando UserDetailsService

--- src/main/java/com/devsipriano/dscommerce/entities/User.java
+++ src/main/java/com/devsipriano/dscommerce/entities/User.java
@@ -123,6 +123,20 @@ public class User implements UserDetails {
         return orders;
     }
 
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public final boolean equals(Object o) {
         if (this == o) return true;
